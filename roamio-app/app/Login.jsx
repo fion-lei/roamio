c296163b15139e049da36d123d13698a6571aa8b
fion-lei <137603599+fion-lei@users.noreply.github.com>
2025-04-03 15:00:25 -0600

    Fix login end point

--- roamio-app/app/Login.jsx
+++ roamio-app/app/Login.jsx
@@ -46,8 +46,8 @@ export default function Login() {
 
     try {
       //const response = await fetch("http://10.0.0.197:3000/login", {
-      const response = await fetch("http://10.0.0.197:3000/login", {
-        method: "POST",
+      const response = await fetch("http://10.0.2.2:3000/login", {
+          method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
       });
